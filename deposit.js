9c6d8ce349f4c4db67d9fea42c5fee1064f900c5
fazlarabbi07 <md.fazlarabbi07@gmail.com>
2022-08-19 12:56:10 +0600

    project is fully completed

--- deposit.js
+++ deposit.js
@@ -1,21 +1,28 @@
 //clicking Deposit  Button
-document.getElementById('btnDeposit').addEventListener('click',function clickDepositBtn(){
+document
+  .getElementById("btnDeposit")
+  .addEventListener("click", function clickDepositBtn() {
+    const depositFieldAmount = document.getElementById("depositField");
+    const previousDepositAmount = document.getElementById(
+      "previousDepositAmount"
+    );
+    const previousBalances = document.getElementById("previousBalance");
 
-const depositFieldAmount=document.getElementById('depositField');
-const previousDepositAmount=document.getElementById('previousDepositAmount');
-const previousBalances=document.getElementById('previousBalance');
+    //Make string to Float
+    const previousDepositAmounts = parseFloat(previousDepositAmount.innerText);
+    const depositFieldsValues = parseFloat(depositFieldAmount.value);
+    const previousBalance = parseFloat(previousBalances.innerText);
+     // Remove the input Value 
+     depositFieldAmount.value='';
+    if (isNaN(depositFieldsValues)) {
+      alert("Please Input a valid Number");
+    } else {
+      //Adding the deposit amount Section
+      const totalDeposit = previousDepositAmounts + depositFieldsValues;
+      previousDepositAmount.innerText = totalDeposit;
 
-//Make string to Float
-const previousDepositAmounts=parseFloat(previousDepositAmount.innerText);
-const depositFieldsValues=parseFloat(depositFieldAmount.value);
-const previousBalance=parseFloat(previousBalances.innerText)
-
-//Adding the deposit amount Section
-const totalDeposit=previousDepositAmounts+depositFieldsValues;
-previousDepositAmount.innerText=totalDeposit;
-
-//Adding The Deposit amout to Main Balance
-const totalBalance=previousBalance+depositFieldsValues;
-previousBalances.innerText=totalBalance;
-
-})
+      //Adding The Deposit amout to Main Balance
+      const totalBalance = previousBalance + depositFieldsValues;
+      previousBalances.innerText = totalBalance;
+    }
+  });
--- withdraw.js
+++ withdraw.js
@@ -8,15 +8,31 @@ document.getElementById('btnWithdraw').addEventListener('click',function clickWi
     //Make string to Float
     const previousWithdrawAmounts=parseFloat(previousWithdrawAmount.innerText);
     const withdrawFieldsValues=parseFloat(withdrawFieldAmount.value);
-    const previousBalance=parseFloat(previousBalances.innerText)
-    
-    //Subtract the Withdraw amount Section
-    const totalWithdraw=previousWithdrawAmounts+withdrawFieldsValues;
-    const totalWithdrawIn=Math.abs(totalWithdraw);
-    previousWithdrawAmount.innerText=totalWithdrawIn;
-    
-    //Subtract The Withdraw amout from Main Balance
-    const totalBalance=previousBalance-withdrawFieldsValues;
-    previousBalances.innerText=totalBalance;
-    
+    const previousBalance=parseFloat(previousBalances.innerText);
+    // Remove the input Value 
+    withdrawFieldAmount.value='';
+    if(isNaN(withdrawFieldsValues))
+    {
+        alert("Please Input a valid Number"); 
+    }
+    else
+    {
+        if(withdrawFieldsValues>previousBalance)
+        {
+            alert("Insufficient Balance");
+        }
+        else
+            {
+        //Subtract the Withdraw amount Section
+        const totalWithdraw=previousWithdrawAmounts+withdrawFieldsValues;
+        const totalWithdrawIn=Math.abs(totalWithdraw);
+        previousWithdrawAmount.innerText=totalWithdrawIn;
+        
+        //Subtract The Withdraw amout from Main Balance
+        const totalBalance=previousBalance-withdrawFieldsValues;
+        previousBalances.innerText=totalBalance;
+            }
+    }
+
+
     })
